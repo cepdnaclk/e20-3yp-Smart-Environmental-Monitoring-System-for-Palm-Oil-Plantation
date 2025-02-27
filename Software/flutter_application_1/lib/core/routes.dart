82a77648353dcc1ab3e1c41872e99cb6db198b51
Methmi Ravindi Perera <150885179+methmi06@users.noreply.github.com>
2025-02-27 19:58:48 +0530

    edited main.dart

--- Software/flutter_application_1/lib/core/routes.dart
+++ Software/flutter_application_1/lib/core/routes.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../presentation/screens/UserLogin.dart';
 import '../presentation/screens/LoginScreen.dart';
+import '../presentation/screens/HomeScreen.dart';
 import '../presentation/screens/SignUpScreen.dart';
 import '../presentation/screens/SoilParametersDisplay.dart';
 import '../presentation/screens/StatisticsScreen.dart';
@@ -9,6 +10,7 @@ import '../presentation/screens/ResetPassword.dart';
 
 class AppRoutes {
   static const String home = '/home';
+  static const String userLogin = '/userLogin';
   static const String login = '/login';
   static const String signup = '/signup';
   static const String soilParameters = '/soilParameters';
@@ -18,8 +20,10 @@ class AppRoutes {
 
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
-      case home:
+      case userLogin:
         return MaterialPageRoute(builder: (_) => UserLogin());
+      case home:
+        return MaterialPageRoute(builder: (_) => HomeScreen());
       case login:
         return MaterialPageRoute(builder: (_) => LoginScreen());
       case signup:
--- Software/flutter_application_1/lib/main.dart
+++ Software/flutter_application_1/lib/main.dart
@@ -1,8 +1,45 @@
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:flutter/material.dart';
+// import 'core/routes.dart';
+// import 'firebase_options.dart';
+//
+// Future<void> main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//
+//   await Firebase.initializeApp(
+//     options: DefaultFirebaseOptions.currentPlatform,
+//   );
+//
+//   FirebaseFirestore.instance.settings = const Settings(
+//     persistenceEnabled: true,
+//   );
+//
+//   runApp(const MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: 'Smart Environmental Monitoring',
+//       theme: ThemeData(
+//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+//         useMaterial3: true,
+//       ),
+//       initialRoute: AppRoutes.userLogin, // Start at Login Screen
+//       onGenerateRoute: AppRoutes.generateRoute, // Route manager
+//     );
+//   }
+// }
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'core/routes.dart';
-import 'firebase_options.dart';
+import 'package:flutter_application_1/firebase_options.dart';
+import 'package:flutter_application_1/presentation/screens/soilParametersDisplay.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -10,7 +47,6 @@ Future<void> main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-
   FirebaseFirestore.instance.settings = const Settings(
     persistenceEnabled: true,
   );
@@ -24,14 +60,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Smart Environmental Monitoring',
+      title: 'Soil Parameters',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         useMaterial3: true,
       ),
-      initialRoute: AppRoutes.home, // Start at Login Screen
-      onGenerateRoute: AppRoutes.generateRoute, // Route manager
+      home: SoilParametersDisplay(), // Use your SoilParameterScreen as the home screen
     );
   }
 }
