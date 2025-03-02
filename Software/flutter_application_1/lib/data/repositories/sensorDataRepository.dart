ac11158356986714dcd6b418992cd3637d177f64
dasunikawya2001 <e20197@eng.pdn.ac.lk>
2025-03-02 15:54:48 +0530

    Device Two Data retrival

--- Software/flutter_application_1/lib/data/repositories/sensorDataRepository.dart
+++ Software/flutter_application_1/lib/data/repositories/sensorDataRepository.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_application_1/domain/entities/SoilParameters.dart';
 import 'package:flutter_application_1/domain/entities/SensorData.dart';
+import 'package:flutter_application_1/domain/entities/DeviceTwoData.dart';
 
 class FirestoreRepository {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
@@ -36,6 +37,22 @@ class FirestoreRepository {
           latitude: (doc['latitude'] ?? 0).toDouble(),
           longitude: (doc['longitude'] ?? 0).toDouble(),
           timestamp: (doc['timestamp'] as Timestamp).toDate(),
+        );
+      }).toList();
+    } catch (e) {
+      throw Exception("Failed to fetch sensor data: $e");
+    }
+  }
+
+    // Fetch Sensor Data - Device-2 from Firestore
+  Future<List<DeviceTwoData>> getDeviceTwoData() async {
+    try {
+      QuerySnapshot snapshot = await _firestore.collection('DeviceTwoData').get();
+      return snapshot.docs.map((doc) {
+        return DeviceTwoData(
+          id: doc.id,
+          lux: (doc['lux'] ?? 0).toDouble(),
+          tipCount: (doc['tipCount'] ?? 0).toDouble(),
         );
       }).toList();
     } catch (e) {
---
+++ Software/flutter_application_1/lib/domain/entities/DeviceTwoData.dart
@@ -0,0 +1,11 @@
+class DeviceTwoData {
+  final String id;
+  final double lux;
+  final int tipCount;
+
+  DeviceTwoData({
+    required this.id,
+    required this.lux,
+    required this.tipCount,
+  });
+}
