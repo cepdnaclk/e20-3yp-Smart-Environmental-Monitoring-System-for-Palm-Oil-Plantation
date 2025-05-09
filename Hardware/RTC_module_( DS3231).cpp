c99b364387c533766683699bd52b30516431328c
Ishan Kumarasinghe <e20211@eng.pdn.ac.lk>
2025-05-09 23:39:07 +0530

    New module testing
    SD card Module
    RTC module
    Documentation

---
+++ Hardware/RTC_module_( DS3231).cpp
@@ -0,0 +1,39 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <RTClib.h>
+
+RTC_DS3231 rtc;
+
+void setup() {
+  Serial.begin(115200);
+  Wire.begin(21, 22);  // SDA, SCL for ESP32
+
+  if (!rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    while (1);
+  }
+
+  if (rtc.lostPower()) {
+    Serial.println("RTC lost power, setting the time!");
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  }
+}
+
+void loop() {
+  DateTime now = rtc.now();
+
+  Serial.print("Current time: ");
+  Serial.print(now.year(), DEC);
+  Serial.print('/');
+  Serial.print(now.month(), DEC);
+  Serial.print('/');
+  Serial.print(now.day(), DEC);
+  Serial.print(" ");
+  Serial.print(now.hour(), DEC);
+  Serial.print(':');
+  Serial.print(now.minute(), DEC);
+  Serial.print(':');
+  Serial.println(now.second(), DEC);
+
+  delay(1000);
+}
